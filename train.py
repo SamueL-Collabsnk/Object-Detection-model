7342b7c95586feb6198bf12beb46a6edbf8459da
Your Name <you@example.com>
2026-06-26 13:03:06 +0300

    all changes

--- train.py
+++ train.py
@@ -6,12 +6,12 @@ model = YOLO("yolov8n.pt")
 results = model.train(
     data = "data.yaml",
     epochs = 50,
-    imgsz = 768,
-    batch = 32,
+    imgsz =640,
+    batch = 16,
     patience = 10,
     project ="runs",
     name = "yolo_experiment",
-    lr0 = 0.0001,
+    lr0 = 0.001,
     weight_decay = 0.0005
 )
 
